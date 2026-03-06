15196bf25dfd47c67888e50bdf0b3ab822d418c6
Shwet Singh <123950334+sss135790@users.noreply.github.com>
2026-03-07 00:20:01 +0530

    Adding CORS

--- server/index.js
+++ server/index.js
@@ -21,12 +21,12 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use( 
-	cors({ 
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:9000"],
+    credentials: true,
+  })
+);
 
 app.use(
 	fileUpload({
